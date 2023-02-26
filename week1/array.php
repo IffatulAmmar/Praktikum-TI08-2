54e51b1e7d5f0b21fa8aaf213f15b0eacfa41927
IffatulAmmar <m.iffatul.a@gmail.com>
2023-02-26 10:21:22 +0700

    praktikum dan tugas minggu 1 praktikum web2

--- week1/array.php
+++ week1/array.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 // =================
 // array indexing
@@ -28,35 +27,5 @@ echo "</ol>";
 
 // array asosiatif
 $buah = ["P"=>'pisang',"J"=>'jambu',"A"=>'pepaya'];
-=======
-<?php
-// =================
-// array indexing
-$ar_buah = ['pisang','jambu','pepaya'];
 
-// mencetak salah satu index array
-echo $ar_buah[2];
-
-// mencetak total daya array
-echo '<br> Jumlah Buah : ' .count($ar_buah);
-
-// menambah data array
-$ar_buah[] = "durian";
-
-// menghapus 1 data index ke-1
-unset($ar_buah[1]); 
-
-// merubah index ke-2 menjadi buah manggis
-$ar_buah[2] = "manggis";
-
-// mencetak seluruh data array
-echo "<ol>";
-foreach ($ar_buah as $buah){
-    echo "<li>$buah</li>";
-}
-echo "</ol>";
-
-// array asosiatif
-$buah = ["P"=>'pisang',"J"=>'jambu',"A"=>'pepaya'];
->>>>>>> 7071fe434baeb69c876afa3b38ab0f290ce4b32d
 ?>
