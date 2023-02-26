a891bd5694b271a16757ddcc99109002d1e86d9a
IffatulAmmar <m.iffatul.a@gmail.com>
2023-02-26 10:18:07 +0700

    praktikum dan tugas minggu 1 praktikum web2

--- week1/array_siswa.php
+++ week1/array_siswa.php
@@ -1,4 +1,3 @@
-
 <?php
 // buat data mahasiswa dan simpan ke dalam array asosiatif
 $ns1 = ['id'=>1,'nim'=>"01101", 'uts'=>80, 'uas'=>90, 'tugas'=>95];
@@ -10,6 +9,7 @@ $ar_nilai = [$ns1, $ns2, $ns3];
 ?>
 <!-- buat table dengan elemen html -->
 <h3 style="text-align:center;">Daftar Nilai Mahasiswa</h3>
+
 <table border="1" width="100%">
     <thead>
         <tr>
@@ -39,43 +39,4 @@ $ar_nilai = [$ns1, $ns2, $ns3];
         ?>
     </tbody>
 
-<?php
-// buat data mahasiswa dan simpan ke dalam array asosiatif
-$ns1 = ['id'=>1,'nim'=>"01101", 'uts'=>80, 'uas'=>90, 'tugas'=>95];
-$ns2 = ['id'=>2,'nim'=>"02202", 'uts'=>85, 'uas'=>93, 'tugas'=>80];
-$ns3 = ['id'=>3,'nim'=>"03303", 'uts'=>87, 'uas'=>95, 'tugas'=>85];
-
-// simpan variable data array asosiatif ke dalam array indexing
-$ar_nilai = [$ns1, $ns2, $ns3];
-?>
-<!-- buat table dengan elemen html -->
-<h3 style="text-align:center;">Daftar Nilai Mahasiswa</h3>
-<table border="1" width="100%">
-    <thead>
-        <tr>
-            <th>NO</th>
-            <th>NIM</th>
-            <th>UTS</th>
-            <th>UAS</th>
-            <th>TUGAS</th>
-            <th>NILAI AKHIR</th>
-        </tr>
-    </thead>
-    <tbody> 
-        <?php
-        $nomor = 1; 
-        foreach($ar_nilai as $nilai){
-        echo '<tr><td>' .$nomor .'</td>';
-        echo '<td>' .$nilai['nim'] .'</td>';
-        echo '<td>' .$nilai['uts'] .'</td>';
-        echo '<td>' .$nilai['uas'] .'</td>';
-        echo '<td>' .$nilai['tugas'] .'</td>';
-        // tuliskan perhitungann nilai akhir disini
-        $rata = ($nilai['uts']+$nilai['uas']+$nilai['tugas'])/3;
-        echo '<td>' .$rata .'</td>';
-        echo '</tr>';
-        $nomor++;
-        }
-        ?>
-    </tbody>
 </table>
