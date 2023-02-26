695472d6c7d7acb18fefd9216f378bd8fe379f37
IffatulAmmar <m.iffatul.a@gmail.com>
2023-02-26 10:12:50 +0700

    praktikum dan tugas minggu 1 pemgrograman web2

---
+++ week1/variable.php
@@ -0,0 +1,34 @@
+<<<<<<< HEAD
+<?php
+$nama = "Data Diri Mahasiswa";
+// Jika nama variable sama tapi memiliki nilai yang berbeda maka yang
+// akan dicetak adalah nilai yang kedua
+echo $nama;
+
+// ==============================================
+// definisi variable data diri
+$nama_saya = "Muhammad Iffatul Ammar";
+$umur = 19;
+$berat = 65;
+
+echo '<br>Nama : ' .$nama_saya;
+echo "<br>Umur saya adalah $umur Tahun";
+echo "<br>Berat saya adalah $berat Kg";
+=======
+<?php
+$nama = "Data Diri Mahasiswa";
+// Jika nama variable sama tapi memiliki nilai yang berbeda maka yang
+// akan dicetak adalah nilai yang kedua
+echo $nama;
+
+// ==============================================
+// definisi variable data diri
+$nama_saya = "Muhammad Iffatul Ammar";
+$umur = 19;
+$berat = 65;
+
+echo '<br>Nama : ' .$nama_saya;
+echo "<br>Umur saya adalah $umur Tahun";
+echo "<br>Berat saya adalah $berat Kg";
+>>>>>>> 7071fe434baeb69c876afa3b38ab0f290ce4b32d
+?> 
---
+++ week1/variable_konstan.php
@@ -0,0 +1,36 @@
+<<<<<<< HEAD
+<?php
+// definisikan varible konstanta
+define('PHI', 3.14);
+
+// soal 1 : Hitunglah Luas Lingkaran dengan jari-jari 8
+// soal 2 : Hitunglah Keliling Lingkaran dengan jari-jari 8
+
+// definisikan variable jari-jari
+$jari = 8;
+$luas = PHI * $jari * $jari;
+$keliling = 2 * PHI * $jari;
+
+echo "Luas Lingkaran dengan jari-jari $jari adalah $luas cm2";
+echo "<br>Keliling Lingkaran dengan jari-jari $jari adalah $keliling cm";
+
+
+=======
+<?php
+// definisikan varible konstanta
+define('PHI', 3.14);
+
+// soal 1 : Hitunglah Luas Lingkaran dengan jari-jari 8
+// soal 2 : Hitunglah Keliling Lingkaran dengan jari-jari 8
+
+// definisikan variable jari-jari
+$jari = 8;
+$luas = PHI * $jari * $jari;
+$keliling = 2 * PHI * $jari;
+
+echo "Luas Lingkaran dengan jari-jari $jari adalah $luas cm2";
+echo "<br>Keliling Lingkaran dengan jari-jari $jari adalah $keliling cm";
+
+
+>>>>>>> 7071fe434baeb69c876afa3b38ab0f290ce4b32d
+?> 
