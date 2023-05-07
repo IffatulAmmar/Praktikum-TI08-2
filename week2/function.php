794751fab5eb8cbe40e76a0c73689eacc2631029
IffatulAmmar <m.iffatul.a@gmail.com>
2023-05-07 12:56:21 +0700

    merapihkan

--- week2/function.php
+++ week2/function.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 function kelulusan ($nilai_rata){
     // jika nilai rata-rata siswa <55 = tidak lulus
@@ -37,44 +36,4 @@ function grade ($nilai_rata){
     }
     else "I (anda tidak ada nilai)";
 }
-=======
-<?php
-function kelulusan ($nilai_rata){
-    // jika nilai rata-rata siswa <55 = tidak lulus
-    if ($nilai_rata <=55){
-        return 'Tidak Lulus';
-    }
-    // jika niai rata-rata siswa >55 = lulus
-    else {
-        return 'Lulus';
-    }
-
-}
-// fungsi grade
-// nilai 85-100 nilai = A
-// nilai 70-84 nilai = B
-// nilai 56-69 nilai = C
-// nilai 36-55 nilai = D
-// nilai 10-35 nilai = E
-// tidak ada nilai = I (anda tidak ada nilai)
-
-function grade ($nilai_rata){
-    if ($nilai_rata >= 85){
-        return 'A';
-    }
-    elseif ($nilai_rata >= 70){
-        return 'B';
-    }
-    elseif ($nilai_rata >= 56){
-        return 'C';
-    }
-    elseif ($nilai_rata >= 36){
-        return 'D';
-    }
-    elseif ($nilai_rata >= 10){
-        return 'E';
-    }
-    else "I (anda tidak ada nilai)";
-}
->>>>>>> 1175c45de0bc81556f2c66acb0309a453e4046b9
 ?>
--- week2/ifelse.php
+++ week2/ifelse.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 $teman = "bambang";
@@ -12,19 +11,4 @@ else {
     echo "bambang bukan teman saya";
 }
 
-=======
-<?php
-
-$teman = "bambang";
-
-// cek terlebih dahulu apakah bambang teman saya atau tidak
-
-if ($teman == "bambang"){
-    echo "bambang adalah teman saya";
-}
-else {
-    echo "bambang bukan teman saya";
-}
-
->>>>>>> 1175c45de0bc81556f2c66acb0309a453e4046b9
 ?>
